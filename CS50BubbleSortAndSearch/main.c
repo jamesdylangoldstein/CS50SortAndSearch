7cbe74b17bca5a3cb2bc3fc6cacd8878ddd24182
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-16 12:24:33 -0700

    Not working, time to clean

--- CS50BubbleSortAndSearch/main.c
+++ CS50BubbleSortAndSearch/main.c
@@ -34,6 +34,10 @@ int sortInts(int sizeOfArray, int arrayOfInts[])
     int lastPositionArray = sizeOfArray - 1;
     int smallestInt;
     int forwardShiftCounter;
+    int testAllDigits;
+    int comparisonCounter;
+    int loopIterations = loopIterationsFunc(sizeOfArray);
+    int lastPositionDup = lastPositionArray;
     
     printf("Unsorted: ");
     for (int printCounter = 0; printCounter < sizeOfArray; printCounter++)
@@ -42,59 +46,82 @@ int sortInts(int sizeOfArray, int arrayOfInts[])
     }
     printf("\n");
     
-    for (int arrayCounter = 0; arrayCounter < sizeOfArray; arrayCounter++)
+    printf("Loop iterations = %i \n", loopIterations);
+    
+    testAllDigits = 0;
+    comparisonCounter = 1;
+
+    while (loopIterations > 0)
     {
-        if (arrayOfInts[0] > arrayOfInts[arrayCounter + 1])
+        for (int arrayCounter = testAllDigits; arrayCounter < sizeOfArray; arrayCounter++)
         {
-            /*
-            // Step 1
-            // ArrayCounter = 0
-            smallestInt = arrayOfInts[arrayCounter + 1];
-            arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
-            arrayOfInts[0] = smallestInt;
-            
-            // Step 2
-            // ArrayCounter = 1
-            smallestInt = arrayOfInts[arrayCounter + 1];
-            arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
-            arrayOfInts[arrayCounter] = arrayOfInts[arrayCounter - 1];
-            arrayOfInts[0] = smallestInt;
-            
-            // Step 3
-            // ArrayCounter = 2
-            smallestInt = arrayOfInts[arrayCounter + 1];
-            arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
-            arrayOfInts[arrayCounter] = arrayOfInts[arrayCounter - 1];
-            arrayOfInts[arrayCounter - 1] = arrayOfInts[arrayCounter - 2];
-            arrayOfInts[0] = smallestInt;
-            
-            // Step 4
-            // ArrayCounter = 3
-            smallestInt = arrayOfInts[arrayCounter + 1];
-            arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
-            arrayOfInts[arrayCounter] = arrayOfInts[arrayCounter - 1];
-            arrayOfInts[arrayCounter - 1] = arrayOfInts[arrayCounter - 2];
-            arrayOfInts[arrayCounter - 2] = arrayOfInts[arrayCounter - 3];
-            arrayOfInts[0] = smallestInt;
-            */
-            
-            //WORKING
-            smallestInt = arrayOfInts[arrayCounter + 1];
-            arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
-            
-            // Shifts digits forward
-            forwardShiftCounter = 0;
-            if (arrayCounter > 0)
+            if (arrayOfInts[testAllDigits] > arrayOfInts[arrayCounter + 1])
             {
-                for (int whatToShiftCounter = 1; whatToShiftCounter < sizeOfArray; whatToShiftCounter++)
+                printf("%i COMP %i.\n", testAllDigits, arrayCounter + 1);
+                /*
+                // Step 1
+                // ArrayCounter = 0
+                smallestInt = arrayOfInts[arrayCounter + 1];
+                arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
+                arrayOfInts[0] = smallestInt;
+                
+                // Step 2
+                // ArrayCounter = 1
+                smallestInt = arrayOfInts[arrayCounter + 1];
+                arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
+                arrayOfInts[arrayCounter] = arrayOfInts[arrayCounter - 1];
+                arrayOfInts[0] = smallestInt;
+                
+                // Step 3
+                // ArrayCounter = 2
+                smallestInt = arrayOfInts[arrayCounter + 1];
+                arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
+                arrayOfInts[arrayCounter] = arrayOfInts[arrayCounter - 1];
+                arrayOfInts[arrayCounter - 1] = arrayOfInts[arrayCounter - 2];
+                arrayOfInts[0] = smallestInt;
+                
+                // Step 4
+                // ArrayCounter = 3
+                smallestInt = arrayOfInts[arrayCounter + 1];
+                arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
+                arrayOfInts[arrayCounter] = arrayOfInts[arrayCounter - 1];
+                arrayOfInts[arrayCounter - 1] = arrayOfInts[arrayCounter - 2];
+                arrayOfInts[arrayCounter - 2] = arrayOfInts[arrayCounter - 3];
+                arrayOfInts[0] = smallestInt;
+                */
+                
+                //WORKING
+                smallestInt = arrayOfInts[arrayCounter + 1];
+                arrayOfInts[arrayCounter + 1] = arrayOfInts[arrayCounter];
+                
+                // Shifts digits forward
+                forwardShiftCounter = 0;
+                if (arrayCounter > 0)
                 {
-                    arrayOfInts[arrayCounter + forwardShiftCounter] = arrayOfInts[arrayCounter - whatToShiftCounter];
-                    forwardShiftCounter--;
+                    for (int whatToShiftCounter = 1; whatToShiftCounter < sizeOfArray; whatToShiftCounter++)
+                    {
+                        arrayOfInts[arrayCounter + forwardShiftCounter] = arrayOfInts[arrayCounter - whatToShiftCounter];
+                        forwardShiftCounter--;
+                    }
+                }
+            
+                comparisonCounter++;
+                
+                arrayOfInts[0] = smallestInt;
+                
+                if (lastPositionDup == 0)
+                {
+                    
+                }
+                else if (comparisonCounter % lastPositionDup == 0)
+                {
+                    testAllDigits++;
+                    comparisonCounter = 1;
+                    lastPositionDup--;
                 }
             }
-        
-            arrayOfInts[0] = smallestInt;
         }
+        loopIterations--;
     }
     
     printf("Sorted: ");
@@ -107,3 +134,14 @@ int sortInts(int sizeOfArray, int arrayOfInts[])
     return 0;
 }
 
+int loopIterationsFunc(int sizeOfArray)
+{
+    int loopIterations = 0;
+    
+    for(int loopIterCounter = 1; loopIterCounter < sizeOfArray; loopIterCounter++)
+    {
+        loopIterations += sizeOfArray - loopIterCounter;
+    }
+    
+    return loopIterations;
+}
