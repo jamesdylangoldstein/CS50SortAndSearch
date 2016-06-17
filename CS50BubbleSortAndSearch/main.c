a9616a364c26b3fce5e460fb79c3d75b2d3ad832
James Goldstein <jamesdylangoldstein@gmail.com>
2016-06-16 22:09:47 -0700

    Working find?

--- CS50BubbleSortAndSearch/main.c
+++ CS50BubbleSortAndSearch/main.c
@@ -11,13 +11,15 @@
 
 int sortInts(int sizeOfArray, int arrayOfInts[]);
 int loopIterationsFunc(int sizeOfArray);
-int searchForInt(int intToFind, int sizeOfArray, int arrayOfInts[]);
+int searchForInt(int intToFind, int startValue, int halfSize, int sizeOfArray, int arrayOfInts[]);
 
 int main(int argc, const char * argv[])
 {
     printf("How many integers would you like sorted:");
     int sizeOfArray = GetInt();
     int arrayOfInts[sizeOfArray + 1];
+    int intToFind;
+    int halfSize = sizeOfArray;
     
     for (int populateCounter = 0; populateCounter < sizeOfArray; populateCounter++)
     {
@@ -40,6 +42,9 @@ int main(int argc, const char * argv[])
         printf("%i ", arrayOfInts[printCounter]);
     }
     printf("\n");
+    printf("Enter the integer you'd like to find\n");
+    intToFind = GetInt();
+    searchForInt(intToFind, 0, halfSize, sizeOfArray, arrayOfInts);
     
     return 0;
 }
@@ -74,7 +79,7 @@ int sortInts(int sizeOfArray, int arrayOfInts[])
             }
             startingPoint++;
             comparisonCounter++;
-
+            
             if (testAllDigitsUppedCounter == (lastPositionArray - 1))
             {
                 return 0;
@@ -89,14 +94,57 @@ int sortInts(int sizeOfArray, int arrayOfInts[])
             }
         }
         loopIterations--;
-        
     }
     
     return 0;
 }
 
-int searchForInt(int intToFind, int sizeOfArray, int arrayOfInts[])
+int searchForInt(int intToFind, int startValue, int halfSize, int sizeOfArray, int arrayOfInts[])
 {
+    halfSize = halfSize/2;
+    printf("halfSize = %i \n", halfSize);
+    printf("startValue = %i \n", startValue);
+    
+    if (halfSize == 0)
+    {
+        printf("%i not found!\n", intToFind);
+        return 0;
+    }
+    int left = startValue + halfSize - 1;
+    int right = startValue + halfSize;
+    
+    printf("comparing %i to %i\n", arrayOfInts[left], arrayOfInts[right]);
+    if (intToFind < arrayOfInts[left])
+    {
+        // Left side
+        printf("It's on the left side\n");
+        searchForInt(intToFind, 0, halfSize, sizeOfArray, arrayOfInts);
+    }
+    else if (intToFind > arrayOfInts[right])
+    {
+        // Right side
+        printf("It's on the right side\n");
+        searchForInt(intToFind, halfSize, halfSize, sizeOfArray, arrayOfInts);
+    }
+    else if (intToFind == arrayOfInts[left])
+    {
+        // We found it
+        printf("%i found in position %i!\n", intToFind, startValue + halfSize);
+        
+    }
+     else if (intToFind == arrayOfInts[right])
+    {
+        // We found it
+        printf("%i found in position %i!\n", intToFind, startValue + halfSize + 1);
+        return 0;
+    }
+    
+    if (halfSize == 1)
+    {
+        printf("%i not found!\n", intToFind);
+        return 0;
+    }
+    
     
     return 0;
 }
